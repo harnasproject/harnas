a2884165b079a80ce2cb04c2597faac7ccdabab1
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-11-28 14:07:18 +0100

    Switched to postgresql. Added sublime-project.

--- harnas/settings.py
+++ harnas/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'harnas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'harnas',
+        'USER': 'harnas',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT':'',
     }
 }
 
