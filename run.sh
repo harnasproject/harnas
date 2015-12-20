34788425da3067dcdd5fb451b5b0b1729aee6eba
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-12-20 15:41:56 +0100

    Updated run script.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+ps aux | grep manage | sed 's/\s\+/ /g' | cut -f2 -d' ' | head -n1 | xargs kill -9
 pip install -r requirements.txt
-./manage.py syncdb
+./manage.py migrate
 ./manage.py runfcgi method=threaded host=127.0.0.1 port=3033
-
 # eralchemy -i 'postgresql+psycopg2://postgres:postgres@localhost:5432/harnas' -o filtered.pdf
