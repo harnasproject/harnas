bdc50151a7a17d15270f2e5c70011a1dacc57eba
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-12-24 00:12:01 +0100

    Added permission check in contest details view.

--- harnas/contest/views.py
+++ harnas/contest/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Contest
+from guardian.decorators import permission_required
 
 def index(request):
     return render(request, 'contest/index.html')
 
+@permission_required('contest.view')
 def details(request, id):
     contest = Contest.objects.get(pk=id)
     return render(request, 'contest/details.html', { 'contest': contest })
