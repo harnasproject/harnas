60df0ef56bbe3c85dc3bdb2f10d71214522294df
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-12-28 20:30:32 +0100

    bleach_whitelist package for easy whitelisting I can't import vars properly, so as workaround I've copied the contents to harnas.contest.

--- harnas/contest/templatetags/pandoc_convert.py
+++ harnas/contest/templatetags/pandoc_convert.py
@@ -3,6 +3,7 @@ from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 import pypandoc
 import bleach
+from harnas.contest.bleach_whitelist import print_tags, print_attrs, all_styles
 
 
 register = template.Library()
@@ -15,7 +16,13 @@ def to_html(text):
         '--mathjax',
         '--smart',
     ]
-    raw_html = pypandoc.convert(text, 'html', format='md', extra_args=pandoc_args)
-    # html = bleach.clean(raw_html)
+    raw_html = pypandoc.convert(text,
+                                'html',
+                                format='md',
+                                extra_args=pandoc_args)
+    html = bleach.clean(raw_html,
+                        print_tags,
+                        print_attrs,
+                        all_styles)
     html = raw_html
     return mark_safe(html)
