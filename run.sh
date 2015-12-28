b583021b6337f61c6f552da71528c17bc79b7697
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-12-28 18:01:47 +0100

    Even better run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-kill `cat harnas.pid`
-rm -f harnas.pid
+ps aux | grep "manage.py" | sed 's/\s\+/ /g' | cut -f2 -d' ' | head -n1 | xargs kill -9
 pip install -r requirements.txt
 ./manage.py migrate
-./manage.py runfcgi method=threaded host=127.0.0.1 port=3033 daemonize=$HARNAS_DAEMONIZE pidfile=harnas.pid
+./manage.py runfcgi method=threaded host=127.0.0.1 port=3033 daemonize=$HARNAS_DAEMONIZE
 # eralchemy -i 'postgresql+psycopg2://postgres:postgres@localhost:5432/harnas' -o filtered.pdf
