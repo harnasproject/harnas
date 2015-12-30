626fd512776c40da4b60df16cc9e0d58bdaa15b6
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-12-30 01:31:57 +0100

    Very basic index, details and edit views for task.

--- harnas/contest/urls.py
+++ harnas/contest/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='contest_index'),
-    url(r'^(?P<id>\d+)/$', views.details, name='contest_details'),
-    url(r'^new/$', views.edit, name='contest_new'),
-    url(r'^edit/(?P<id>\d+)/$', views.edit, name='contest_edit'),
+    url(r'^contest/$', views.index, name='contest_index'),
+    url(r'^contest/(?P<id>\d+)/$', views.details, name='contest_details'),
+    url(r'^contest/new/$', views.edit, name='contest_new'),
+    url(r'^contest/edit/(?P<id>\d+)/$', views.edit, name='contest_edit'),
+    url(r'^task/$', views.task_index, name='task_index'),
+    url(r'^task/(?P<id>\d+)/$', views.task_details, name='task_details'),
+    url(r'^task/new$', views.task_edit, name='task_new'),
+    url(r'^task/edit/(?P<id>\d+)/$', views.task_edit, name='task_edit'),
 ]
