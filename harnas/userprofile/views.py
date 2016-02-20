478ea9f90a76347a8c004f53b688a1e97a2be859
Kamil Klimala <klimalakamil@gmail.com>
2016-02-20 20:02:27 +0100

    fixed a bug with invalid edit profile forms not showing again after submission

--- harnas/userprofile/views.py
+++ harnas/userprofile/views.py
@@ -1,15 +1,16 @@
+from datetime import date
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from django.views.decorators.http import require_GET
-from datetime import date
+from django.views.decorators.http import require_safe
 
 from harnas.userprofile.forms import UserFieldsForm, UserProfileEditForm
 
 
-@require_GET
+@require_safe
 def show(request, user_id):
     try:
         user = User.objects.get(id=user_id)
@@ -18,7 +19,8 @@ def show(request, user_id):
         return render(request, 'profile.html', {
             'user': user,
             'profile': user.userprofile,
-            'age': today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+            'age': today.year - born.year - (
+                (today.month, today.day) < (born.month, born.day))
         })
     except User.DoesNotExist:
         raise Http404
@@ -27,24 +29,33 @@ def show(request, user_id):
 @login_required
 def edit(request):
     user = request.user
+
     if request.method == 'POST':
         user_form = UserFieldsForm(request.POST or None, instance=user)
-        if user_form.is_valid():
-            user_form.save()
-
         profile_form = UserProfileEditForm(
               request.POST or None,
               instance=user.userprofile
         )
-        if profile_form.is_valid():
-            profile_form.save()
 
-        return HttpResponseRedirect('/accounts/' + str(user.id))
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect('/accounts/' + str(user.id))
+        else:
+            return render_to_response(
+                  'edit_profile.html',
+                  {
+                      'user': user,
+                      'user_form': user_form,
+                      'profile_form': profile_form
+                  },
+                  context_instance=RequestContext(request)
+            )
     else:
         return render_to_response(
               'edit_profile.html',
               {
-                  'user': request.user,
+                  'user': user,
                   'user_form': UserFieldsForm(instance=user),
                   'profile_form': UserProfileEditForm(
                         instance=user.userprofile
