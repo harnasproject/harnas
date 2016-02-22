273a598f2a3b97a3e5322b34a5ed1e3880fede9a
Kamil Klimala <klimalakamil@gmail.com>
2016-02-22 21:13:31 +0100

    added messages after successful operations, added password reset and change support

--- harnas/customregistration/urls.py
+++ harnas/customregistration/urls.py
@@ -1,9 +1,48 @@
 from django.conf.urls import include, url
-
-from harnas.customregistration.views import RegistrationView
+from harnas.customregistration.views import *
 
 urlpatterns = [
-    # redirect registration requests to custom view
-    url(r'^register/$', RegistrationView.as_view()),
-    url(r'^', include('registration.backends.default.urls')),
+    # auth patterns to prevent grappelli from overriding them
+    url(
+        r'^login/$',
+        login_override,
+        name='login'
+    ),
+    url(
+        r'^logout/$',
+        logout_override,
+        name='logout'
+    ),
+    url(
+        r'^password/reset/done/$',
+        password_reset_done_override,
+        name='password_reset_done'
+    ),
+    url(
+        r'^reset/done/$',
+        password_reset_complete_override,
+        name='password_reset_complete'
+    ),
+    url(
+        r'^password/change/done/$',
+        password_change_done_override,
+        name='password_change_done'
+    ),
+
+    # redirect registration requests to custom view (displaying messages)
+    url(
+        r'^register/$',
+        RegistrationView.as_view(),
+        name='registration_register'
+    ),
+    # redirects activation requests to custom view (displaying messages)
+    url(
+        r'^activate/complete/$',
+        activation_complete_override,
+        name='registration_activation_complete'
+    ),
+    url(
+        r'^',
+        include('registration.backends.default.urls')
+    )
 ]
--- harnas/customregistration/views.py
+++ harnas/customregistration/views.py
@@ -1,4 +1,61 @@
+from django.contrib import messages
+from django.contrib.auth.views import password_reset, password_reset_confirm, \
+    password_change, login, logout
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext as _
 from registration.backends.default import views
+
+
+def login_override(request):
+    """
+    Change default login view to display message after successful login.
+    """
+    response = login(
+        request,
+        template_name='custom_login.html'
+    )
+    # Base login method returns HttpResponseRedirect upon successful login
+    if request.method == 'POST' and isinstance(response, HttpResponseRedirect):
+        messages.add_message(request, messages.SUCCESS, _('You are now logged in.'))
+    return response
+
+
+def logout_override(request, next_page=None):
+    """
+    Change default logout view to display message after successful logout.
+    """
+    response = logout(request, next_page)
+    if not isinstance(response, HttpResponseRedirect):
+        messages.add_message(request, messages.SUCCESS, _('You have been logged out.'))
+        return HttpResponseRedirect(reverse('homepage'))
+    else:
+        return response
+
+
+def password_reset_done_override(request):
+    messages.add_message(request, messages.INFO, _(
+        'We have sent you an email with a link to reset your password.'))
+    return HttpResponseRedirect(reverse('homepage'))
+
+
+def password_reset_complete_override(request):
+    messages.add_message(request, messages.SUCCESS,
+                         _('Your password has been reset. You can log in.'))
+    return HttpResponseRedirect(reverse('auth_login'))
+
+
+def password_change_done_override(request):
+    messages.add_message(request, messages.SUCCESS,
+                         _('Your password has been changed!'))
+    return HttpResponseRedirect(reverse('account_settings'))
+
+
+def activation_complete_override(request):
+    messages.add_message(request, messages.SUCCESS,
+                         _('Your account is now activated. You can log in.'))
+    return HttpResponseRedirect(reverse('login'))
 
 
 class RegistrationView(views.RegistrationView):
