eb8e185b9723b903086d5186fd3b3d112a2985d0
Kamil Klimala <klimalakamil@gmail.com>
2016-02-22 21:11:19 +0100

    changed password reset link to password change link

--- harnas/userprofile/urls.py
+++ harnas/userprofile/urls.py
@@ -4,5 +4,5 @@ from harnas.userprofile import views
 
 urlpatterns = [
     url(r'^(?P<user_id>\d+)/$', views.show, name='account_profile'),
-    url(r'settings/$', views.edit, name='account_settings'),
+    url(r'^settings/$', views.edit, name='account_settings'),
 ]
