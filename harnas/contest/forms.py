071503c835ccc8820fde6aa7b57256c27a479f94
Kamil Klimala <klimalakamil@gmail.com>
2016-03-03 14:50:09 +0100

    Moved forms to separate file

---
+++ harnas/contest/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.forms import ModelForm
+
+from harnas.contest.models import TestCase, Task, News, Contest
+
+
+class TestCaseForm(ModelForm):
+    class Meta:
+        model = TestCase
+        fields = ['task', 'max_memory', 'max_duration', 'comparator',
+                  'executor', 'comparator', 'in_file_path', 'out_file_path']
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label="Upload new file")
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name', 'short_name', 'description', 'test_environment']
+
+
+class NewsForm(ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'description']
+
+
+class ContestForm(ModelForm):
+    class Meta:
+        model = Contest
+        fields = ['name', 'description']
