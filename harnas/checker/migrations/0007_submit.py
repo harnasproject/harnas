dbbeb401f12a84123b32dd2b44e971752049e7fd
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-03-30 10:52:20 +0200

    Sketch of Submit model.

---
+++ harnas/checker/migrations/0007_submit.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contest', '0019_auto_20160308_1441'),
+        ('checker', '0006_auto_20160305_1618'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Submit',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('submitted', models.DateTimeField(auto_now_add=True)),
+                ('solution', models.BinaryField()),
+                ('status', models.CharField(choices=[('QUE', 'In checking queue'), ('MEM', 'Memory limit exceeded'), ('TLE', 'Time limit exceeded'), ('OK', 'Accepted'), ('INT', 'Internal error'), ('ANS', 'Wrong answer'), ('CMP', 'Compilation error'), ('RTE', 'Runtime error')], max_length=3)),
+                ('submitter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('task', models.ForeignKey(to='contest.Task')),
+            ],
+        ),
+    ]
--- harnas/checker/models.py
+++ harnas/checker/models.py
@@ -31,3 +31,31 @@ class TestEnvironmentForm(ModelForm):
     class Meta:
         model = TestEnvironment
         fields = ['summary', 'description']
+
+
+class Submit(models.Model):
+    QUEUED = "QUE"
+    MEMORY_LIMIT = "MEM"
+    TIME_LIMIT = "TLE"
+    ACCEPTED = "OK"
+    INTERNAL_ERROR = "INT"
+    WRONG_ANSWER = "ANS"
+    COMPILATION_ERROR = "CMP"
+    RUNTIME_ERROR = "RTE"
+    STATUS_CHOICES = (
+        (QUEUED, "In checking queue"),
+        (MEMORY_LIMIT, "Memory limit exceeded"),
+        (TIME_LIMIT, "Time limit exceeded"),
+        (ACCEPTED, "Accepted"),
+        (INTERNAL_ERROR, "Internal error"),
+        (WRONG_ANSWER, "Wrong answer"),
+        (COMPILATION_ERROR, "Compilation error"),
+        (RUNTIME_ERROR, "Runtime error"),
+    )
+
+    submitter = models.ForeignKey(User)
+    submitted = models.DateTimeField(auto_now_add=True)
+    task = models.ForeignKey('contest.Task')
+    solution = models.BinaryField()
+    status = models.CharField(max_length=3,
+                              choices=STATUS_CHOICES)
