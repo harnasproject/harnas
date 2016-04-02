5c7aafa7b14f28c5b0723e2bdb15f54ad1138b18
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-04-02 21:06:01 +0200

    Refactored urlpatterns to be cleaner.

--- harnas/checker/urls.py
+++ harnas/checker/urls.py
@@ -1,19 +1,23 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from harnas.checker.views import cluster, template, test_environment
+
+test_environment_urlpatterns = [
+    url(r'^$',
+        test_environment.index,
+        name='test_environment_index'),
+    url(r'^new$',
+        test_environment.edit,
+        name='test_environment_new'),
+    url(r'^(?P<id>\d+)/$',
+        test_environment.details,
+        name='test_environment_details'),
+    url(r'^(?P<id>\d+)/edit$',
+        test_environment.edit,
+        name='test_environment_edit'),
+]
 
 urlpatterns = [
     url(r'^status/$', cluster.status, name='cluster_status'),
     url(r'^template/$', template.index, name='template_index'),
-    url(r'^test_environment/$',
-        test_environment.index,
-        name='test_environment_index'),
-    url(r'^test_environment/(?P<id>\d+)/$',
-        test_environment.details,
-        name='test_environment_details'),
-    url(r'^test_environment/new$',
-        test_environment.edit,
-        name='test_environment_new'),
-    url(r'^test_environment/edit/(?P<id>\d+)/$',
-        test_environment.edit,
-        name='test_environment_edit'),
+    url(r'test_environment/', include(test_environment_urlpatterns)),
 ]
