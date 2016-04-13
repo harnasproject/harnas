0bc4226540546822df1458104f284f29f1b0200e
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-04-13 20:24:28 +0200

    Refactored task urls.

--- harnas/contest/views/task.py
+++ harnas/contest/views/task.py
@@ -49,19 +49,19 @@ def details(request, task_id):
         submit_form = SubmitForm(contest=task.contest)
 
     return render(request, 'contest/task_details.html', {
-                  'task': task,
-                  'task_files': task_files,
-                  'upload_file_form': upload_file_form,
-                  'submit_form': submit_form
-           })
+        'task': task,
+        'task_files': task_files,
+        'upload_file_form': upload_file_form,
+        'submit_form': submit_form
+    })
 
 
 @require_http_methods(['GET', 'POST'])
 @login_required
-def edit(request, id=None):
-    if id:
-        task = Task.objects.get(pk=id)
-        form_post = reverse('task_edit', args=[id])
+def edit(request, task_id=None):
+    if task_id:
+        task = Task.objects.get(pk=task_id)
+        form_post = reverse('task_edit', args=[task_id])
         if not request.user.has_perm('contest.edit_task', task):
             raise PermissionDenied
     else:
@@ -75,7 +75,7 @@ def edit(request, id=None):
         form = TaskForm(instance=task)
     if form.is_valid():
         new_task = form.save(commit=False)
-        if id is None:
+        if task_id is None:
             new_task.author_id = request.user.pk
         new_task.save()
         assign_perm('contest.edit_task', request.user, new_task)
@@ -85,21 +85,22 @@ def edit(request, id=None):
         cache.delete(cache_key)
         return HttpResponseRedirect(reverse('task_details',
                                             args=[new_task.pk]))
-    return render(request, 'contest/task_new.html',
-                  { 'form': form,
-                    'form_post': form_post })
+    return render(request, 'contest/task_new.html', {
+        'form': form,
+        'form_post': form_post
+    })
 
 
 @require_http_methods(['POST'])
 @login_required
-def upload_file(request, id):
-    task = Task.objects.get(pk=id)
+def upload_file(request, task_id):
+    task = Task.objects.get(pk=task_id)
     if not request.user.has_perm('contest.edit_task', task):
         raise PermissionDenied
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
         file = request.FILES['file']
-        task_path = str(id) + '/'
+        task_path = str(task_id) + '/'
         task_filesystem.save(task_path + file.name, file)
     return HttpResponseRedirect(reverse('task_details',
                                         args=[task.pk]))
@@ -107,14 +108,16 @@ def upload_file(request, id):
 
 @require_safe
 @login_required
-def download_file(request, id):
-    task = Task.objects.get(pk=id)
+def download_file(request, task_id):
+    task = Task.objects.get(pk=task_id)
     if not request.user.has_perm('contest.edit_task', task):
         raise PermissionDenied
-    task_path = str(id) + '/'
+    task_path = str(task_id) + '/'
     filename = request.GET.get('filename', None)
     file_path = task_path + filename
-    file_exists = False if filename is None else task_filesystem.exists(file_path)
+    file_exists = False
+    if filename is not None:
+        file_exists = task_filesystem.exists(file_path)
     if not file_exists:
         raise Http404
     file = task_filesystem.open(task_path + filename)
@@ -125,14 +128,16 @@ def download_file(request, id):
 
 @require_safe
 @login_required
-def delete_file(request, id):
-    task = Task.objects.get(pk=id)
+def delete_file(request, task_id):
+    task = Task.objects.get(pk=task_id)
     if not request.user.has_perm('contest.edit_task', task):
         raise PermissionDenied
-    task_path = str(id) + '/'
+    task_path = str(task_id) + '/'
     filename = request.GET.get('filename', None)
     file_path = task_path + filename
-    file_exists = False if filename is None else task_filesystem.exists(file_path)
+    file_exists = False
+    if filename is not None:
+        file_exists = task_filesystem.exists(file_path)
     if not file_exists:
         raise Http404
     task_filesystem.delete(file_path)
